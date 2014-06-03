75e3cde0d45b5b07e917961a0f68d80416c055c6
augustine91 <aguztin20@gmail.com>
2014-06-03 13:31:28 -0600

    viva el chiky

--- code/class 8/class8/src/class8/Class8.java
+++ code/class 8/class8/src/class8/Class8.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package class8;
+
+import java.util.Scanner;
 
 /**
  *
@@ -17,6 +18,35 @@ public class Class8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner OScanner = new Scanner(System.in);
+        int option = 0;
+        String GuionPalabra;
+        String cadena;
+        do {
+            System.out.println("digite el opcion : ");
+            System.out.println("1.Evaluar una palabra");
+            System.out.println("2.evaluar una cadena ");
+            option = OScanner.nextInt();
+            switch (option) {
+                case 1:
+                    OScanner = new Scanner(System.in);
+                    System.out.println("Digite la palabra a evaluar");
+                    ejercicio1 pejercicio1 = new ejercicio1();
+                    pejercicio1.setCadena(OScanner.nextLine());
+                    System.out.println(pejercicio1.alrevez());
+                    System.out.println(pejercicio1.palindromo());
+                case 2:
+                    OScanner = new Scanner(System.in);
+                    System.out.println("Digete la cadena a evaluar");
+                    ejercicio1 jejercicio1 = new ejercicio1();
+                    jejercicio1.setCadena(OScanner.nextLine());
+                    System.out.println("Digite la palabra a buscar");
+                    GuionPalabra = OScanner.nextLine();
+                    System.out.println(jejercicio1.buscapalabra(GuionPalabra));
+
+            }
+
+        } while (option > 0);
     }
-    
+
 }
--- code/class 8/class8/src/class8/ejercicio1.java
+++ code/class 8/class8/src/class8/ejercicio1.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package class8;
 
 /**
@@ -11,5 +10,78 @@ package class8;
  * @author Home
  */
 public class ejercicio1 {
-    
+
+    private String cadena;
+    private String buscapalabra;
+
+    public String alrevez() {
+        String retorno = "";
+        for (int i = getCadena().length() - 1; i >= 0; i--) {
+            retorno += getCadena().charAt(i);
+
+        }
+        return retorno;
+    }
+
+    public String palindromo() {
+        String PalabraAlrevez = "";
+        String cadena;
+        String Cadena2 = "";
+
+        PalabraAlrevez = getCadena();
+
+        for (int i = getCadena().length() - 2; i >= 0; i--) {
+            PalabraAlrevez += getCadena().charAt(i);
+
+        }
+
+        return PalabraAlrevez;
+
+    }
+
+    public String buscapalabra(String pPalabra) {
+        String mensaje = "";
+        String Subcadena = "";
+        int inicioLetra = 0, finalLetra;
+        for (int i = 0; i < getCadena().length(); i++) {
+            if (getCadena().charAt(i) == ' ' || getCadena().length() - 1 == i) {
+                finalLetra = i;
+                if (getCadena().length() - 1 == i) {
+                    Subcadena = getCadena().substring(inicioLetra);
+
+                } else {
+                    Subcadena = getCadena().substring(inicioLetra, finalLetra);
+
+                } if(pPalabra.equals(Subcadena)){
+                    mensaje+="-"+Subcadena+' ';
+                    
+                    
+                } else {
+                    mensaje+=Subcadena+" ";
+                    inicioLetra=i+1;
+                }
+                    
+
+            }
+
+        }
+
+        
+        return mensaje;
+
+    }
+
+    /**
+     * @return the cadena
+     */
+    public String getCadena() {
+        return cadena;
+    }
+
+    /**
+     * @param cadena the cadena to set
+     */
+    public void setCadena(String cadena) {
+        this.cadena = cadena;
+    }
 }
